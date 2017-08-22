620522513b395f6b54e0f15e4fb428f0f621023a
Philippe Duval <philippe@duvalsoft.org>
2017-08-22 16:53:18 +0100

    Update docs for 2fc3d47b65904878d85ebc32a7b65454647ed495
    Fix doc fragments

--- implementors/core/fmt/trait.Binary.js
+++ implementors/core/fmt/trait.Binary.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["serde"] = [];
+implementors["syn"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
